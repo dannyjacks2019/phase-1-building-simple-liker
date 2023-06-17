ed9919fc393efc33ac9691e46358dbdf4ca2bbc8
Dan Jackson <dannyjacks2019@gmail.com>
2023-06-16 21:24:41 -0400

    simple liker

--- main.js
+++ main.js
@@ -3,9 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const articleHeart = document.querySelectorAll(".like-glyph")
+function likeCallBack(e){
+  const heart = e.target
+  mimicServerCall("bogusUrl")
+  .then(function() {
+    if(heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART
+    heart.className = "activated-heart"
+    }
+    else{
+       heart.innerText = EMPTY_HEART
+      heart.className = ""
+    }
+    })
+    .catch((error)=>{
+      const modal = document.getElementById("modal")
+      modal.className = ""
+      modal.innerHTML= error
+      setTimeout(()=> modal.className = "hidden", 3000)
+    })
+  }
+for(const post of articleHeart){
+post.addEventListener("click", likeCallBack)
+  
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
